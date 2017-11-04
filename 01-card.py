dd56677e54018693b3add5052ae656543e30e4c1
ciphern <gjw098@163.com>
2017-11-04 11:20:03 +0800

    card system

---
+++ 01-card.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+name = input("请输入你的姓名:")
+Tel = input("请输入你的电话:")
+Email = input("请输入你的邮箱:")
+QQ = input("请输入你的QQ:")
+
+print("姓名:%s\nTel:%s\nEmail:%s\nQQ:%s"%(name,Tel,Email,QQ))
+
